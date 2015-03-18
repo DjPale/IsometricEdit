d638548af0ffdd2f7b0b701147cb64cfe31f65ca
dj_pale <espen.breivik@gmail.com>
2015-03-18 18:48:53 -0400

    Added linux build script and linux build

---
+++ build_linux.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+haxelib run flow build linux
+ver=`grep version project.flow | cut -d"'" -f2`
+cd bin
+mv linux IsometricEdit-${ver}
+tar -cvzf IsometricEdit-${ver}-linux.tar.gz IsometricEdit-*
+cd ..
+rm builds/*-linux.tar.gz
+mv bin/IsometricEdit-*.tar.gz builds/
+rm -rf bin/IsometricEdit-*
