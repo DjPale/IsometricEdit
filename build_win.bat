88c9ccaf3d34f2c10a2e33e9ce0fa205f622e2e9
Espen Breivik <espen.breivik@gmail.com>
2015-03-18 23:19:42 +0100

    Added windows file and build script

---
+++ build_win.bat
@@ -0,0 +1,9 @@
+@echo off
+haxelib run flow build windows
+for /f "tokens=2 delims='" %%a in ('grep version project.flow') do set ver=%%a
+cd bin/
+ren windows IsometricEdit-%ver%
+"c:\Program Files\7-Zip\7z.exe" a IsometricEdit-%ver%-win.zip IsometricEdit*\
+cd ..
+del builds\*-win.zip
+move bin\IsometricEdit-%ver%-win.zip builds\
